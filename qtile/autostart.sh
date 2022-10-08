c724817ad6fa7658f7940e38e5473746dacc057b
Marco Bazzani <mbazzani@passthrough.mozmail.com>
2022-10-08 11:50:42 -0400

    Initial qtile configuration (For real this time)

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,1 +1,9 @@
-autostart.sh
+#!/bin/bash
+picom --vsync &
+
+#Output name changes on reboot, this way at least one will be activated
+xrandr --output HDMI-1-0 --mode 2560x1440 &
+xrandr --output HDMI-1-1 --mode 2560x1440 &
+xrandr --output HDMI-1-2 --mode 2560x1440 &
+emacs --daemon &
+lxpolkit &
