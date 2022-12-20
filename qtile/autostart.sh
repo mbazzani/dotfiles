c01c35f3983dd04aecfa03dfd5988049da744fa7
mbazzani <114893351+mbazzani@users.noreply.github.com>
2022-12-20 09:58:01 -0500

    Removed emacs command and added wallpaper setting

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 picom --vsync &
 setxkbmap -option caps:escape
+nitrogen --set-auto --random /usr/share/backgrounds/
 
 #Output name changes on reboot, this way at least one will be activated
 xrandr --output HDMI-1-0 --mode 2560x1440 &
 xrandr --output HDMI-1-1 --mode 2560x1440 &
 xrandr --output HDMI-1-2 --mode 2560x1440 &
-emacs --daemon &
+
 picom &
 lxpolkit &
-syncthing serve& 
